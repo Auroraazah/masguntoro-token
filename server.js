274e6c4a5e5003c17b9d7d1046044dd2c431dea4
Auroraazah <arorahputri3@gmail.com>
2026-09-15 22:04:40 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { RtcTokenBuilder, RtcRole } = require('agora-token');
+
+const app = express();
+app.use(cors());
+
+// GANTI DENGAN PUNYA KAMU
+const APP_ID = "cf22b9c1495d4cc89b36a9a81f9cfa16";
+const APP_CERTIFICATE = "isi-certificate-asli-kamu-disini";
+
+app.get('/', (req,res)=> res.send('Masguntoro Token Server Jalan'));
+
+app.get('/token', (req, res) => {
+  const channel = req.query.channel;
+  if (!channel) return res.status(400).json({error: "channel?"});
+
+  const uid = 0;
+  const role = RtcRole.PUBLISHER;
+  const expire = 3600;
+  const now = Math.floor(Date.now() / 1000);
+  
+  const token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, channel, uid, role, now + expire);
+  res.json({ token, appId: APP_ID });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('jalan di '+PORT));
